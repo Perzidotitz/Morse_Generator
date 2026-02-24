6d9b14e5895d90dcb003c05c760fa16610042f3a
Mathys Georgeais <mgeorgea@ptb-03240157.irisa.fr>
2026-02-24 13:46:19 +0100

    added documentation to the readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Morse Generator
 
-Yes so basically just put your input in app and zou
+Yes so basically just put your input in app.py and zou get your generated morse code in the outout folder
--- source/app.py
+++ source/app.py
@@ -1,7 +1,6 @@
 import Morse_Generator as mg
 
 input = "Hi Empenn ! Where do you eat today ? :knife_fork_plate: Supélec :bento: IRISA :green_salad: Biocoop :hospital: CHU :wave: not with the team"
-
 p_csv = "morse.csv"
 p_output = "output/output.wav"
 
